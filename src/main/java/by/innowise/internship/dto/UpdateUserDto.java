03453cf3952a217e0fb7bf647e0da63193ff1f8c
Mikita <MikitaStain@list.ru>
2022-01-25 14:42:02 +0300

    update Specifications

---
+++ src/main/java/by/innowise/internship/dto/UpdateUserDto.java
@@ -0,0 +1,23 @@
+package by.innowise.internship.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateUserDto {
+
+    private String name;
+
+    private String lastName;
+
+    private String password;
+
+    private Long positionId;
+
+    private Long courseId;
+}
